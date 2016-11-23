313dd4921176063a96518c66a304f969869c1147
王耕洲 <wanggengzhou@meituan.com>
2016-11-24 00:41:42 +0800

    Update: 解决 git pull 花费太多时间而导致 expect 超时的问题。

--- run_update_hosts.sh
+++ run_update_hosts.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # 执行 ./update_hosts.sh 后自动输入密码。
 
-./inputPassword.exp ./update_hosts/update_hosts.sh
+echo $PWD
+PWD_ORI=$PWD
+cd ../project/hosts/
+git pull origin master
+
+$PWD_ORI/inputPassword.exp $PWD_ORI/update_hosts/update_hosts.sh $PWD_ORI
--- update_hosts/update_hosts.sh
+++ update_hosts/update_hosts.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo $PWD
-PWD_ORI=$PWD
-cd ../project/hosts/
-git pull origin master
 sudo cp hosts /etc/hosts
