2742c2cea034f27df5f454f4b76c10d6e9304bee
王耕洲 <wanggengzhou@meituan.com>
2016-11-26 14:35:40 +0800

    Update: 更新目录地址。

--- run_update_hosts.sh
+++ run_update_hosts.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # 执行 ./update_hosts.sh 后自动输入密码。
 
-PWD_ORI=$(dirname "$0")
+# 如果只是 dirname "$0"，在当前目录下显示的是点（.）
+PWD_ORI=$(cd $(dirname "$0"); pwd)
+echo $PWD_ORI
 cd ../project/hosts/
 git pull origin master
 
